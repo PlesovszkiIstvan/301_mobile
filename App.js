6b38f68c5abf79266f9b369f7ce55e53d734ed08
andoattila698@gmail.com <kilot58@gmail.com>
2024-02-21 10:35:02 +0100

    kesz

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View,TextInput } from 'react-native';
+import { useState } from 'react';
+
 
 export default function App() {
+  const [height,setHeight] = useState(0)
+  const [radius,setRadius] = useState(0)
+  const [volume,setVolume] = useState(0)
+
+  function calc(){
+    return 1/3*radius*height 
+  }
+
+  function setVol(){
+    let res = calc()
+    setVolume(res)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Terkup szamitas :3</Text>
+      <View style={styles.inputStyles}>
+        <Text>Magasság:</Text>
+        <TextInput 
+        style={styles.heightInput}
+        onChangeText={height => {setHeight(height)}}
+        ></TextInput>
+        <Text>Sugár:</Text>
+        <TextInput 
+        style={styles.radiusInput}
+        onChangeText={radius => {setRadius(radius)}}
+        ></TextInput>
+      </View>
+      <Pressable style={styles.pressable} onPress={setVol}>
+        <Text>Számít</Text>
+      </Pressable>
+      <TextInput style={styles.eredmeny} value={volume}></TextInput>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +44,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'gray',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heightInput: {
+    backgroundColor: '#fff'
+  },
+  radiusInput: {
+    backgroundColor: '#fff'
+  },
+
 });
+
